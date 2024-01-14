f31fa17196fc16ec065cffbcff99f534c5a52aab
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2024-01-14 02:58:02 -0800

    Add table sorting, visual improvements

--- src/routes/domains/+page.svelte
+++ src/routes/domains/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
     import type { PageData } from "./$types";
     import { relativeTimeFromDate } from "$lib/time";
-    import { TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, TableSearch } from "flowbite-svelte";
+    import type { Domain } from "$lib/database";
+    import { DomainStatus } from "$lib/database";
+    import {
+        TableBody,
+        TableBodyCell,
+        TableBodyRow,
+        TableHead,
+        TableHeadCell,
+        TableSearch,
+        Indicator,
+        Badge,
+    } from "flowbite-svelte";
+    import { writable } from "svelte/store";
 
     export let data: PageData;
 
@@ -9,25 +21,104 @@
     $: filteredItems = data.domains.filter(
         (domain) => domain.domain.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1,
     );
+
+    const sortKey = writable<keyof Domain>("created_at"); // default sort key
+    const sortDirection = writable(-1); // default sort direction (descending)
+    $: sortedItems = writable(filteredItems); // make a copy of the items array
+
+    // Define a function to sort the items
+    const sortTable = (key: keyof Domain) => {
+        // If the same key is clicked, reverse the sort direction
+        if ($sortKey === key) {
+            sortDirection.update((val) => -val);
+        } else {
+            sortKey.set(key);
+            sortDirection.set(1);
+        }
+    };
+
+    $: {
+        const key = $sortKey;
+        const direction = $sortDirection;
+        const sorted = [...$sortedItems].sort((a, b) => {
+            const aVal = a[key];
+            const bVal = b[key];
+            if (!aVal || !bVal) {
+                return 0;
+            }
+            if (aVal < bVal) {
+                return -direction;
+            } else if (aVal > bVal) {
+                return direction;
+            }
+            return 0;
+        });
+        sortedItems.set(sorted);
+    }
 </script>
 
 <div class="flex flex-col items-center min-h-screen">
     <TableSearch placeholder="Search domains" striped={true} bind:inputValue={searchTerm}>
         <TableHead>
-            <TableHeadCell>Domain</TableHeadCell>
-            <TableHeadCell>Status</TableHeadCell>
-            <TableHeadCell>Threat type</TableHeadCell>
-            <TableHeadCell>Host</TableHeadCell>
-            <TableHeadCell>Created</TableHeadCell>
-            <TableHeadCell>Updated</TableHeadCell>
+            <TableHeadCell
+                class="dark:hover:text-white transition-all cursor-pointer"
+                on:click={() => sortTable("domain")}>Domain</TableHeadCell
+            >
+            <TableHeadCell
+                class="dark:hover:text-white transition-all cursor-pointer"
+                on:click={() => sortTable("status")}>Status</TableHeadCell
+            >
+            <TableHeadCell
+                class="dark:hover:text-white transition-all cursor-pointer"
+                on:click={() => sortTable("threat_type")}>
+                Threat type
+                </TableHeadCell
+            >
+            <TableHeadCell
+                class="dark:hover:text-white transition-all cursor-pointer"
+                on:click={() => sortTable("host")}>Host</TableHeadCell
+            >
+            <TableHeadCell
+                class="dark:hover:text-white transition-all cursor-pointer"
+                on:click={() => sortTable("created_at")}>Created</TableHeadCell
+            >
+            <TableHeadCell
+                class="dark:hover:text-white transition-all cursor-pointer"
+                on:click={() => sortTable("updated_at")}>Updated</TableHeadCell
+            >
         </TableHead>
-        <TableBody class="divide-y">
-            {#each filteredItems as domain}
+        <TableBody>
+            {#each $sortedItems as domain}
                 <TableBodyRow>
                     <TableBodyCell><a href={domain.url}>{domain.domain}</a></TableBodyCell>
-                    <TableBodyCell>{domain.status}</TableBodyCell>
-                    <TableBodyCell>{domain.threat_type}</TableBodyCell>
-                    <TableBodyCell>{domain.host}</TableBodyCell>
+                    <TableBodyCell>
+                        <Badge
+                            color={domain.status === DomainStatus.inactive
+                                ? "green"
+                                : domain.status === DomainStatus.active
+                                  ? "red"
+                                  : "dark"}
+                            rounded
+                        >
+                            <Indicator
+                                color={domain.status === DomainStatus.inactive
+                                    ? "green"
+                                    : domain.status === DomainStatus.active
+                                      ? "red"
+                                      : "gray"}
+                                size="xs"
+                                class="me-1"
+                            />{domain.status ? domain.status.charAt(0).toUpperCase() + domain.status.substring(1) : "Unknown"}
+                        </Badge>
+                    </TableBodyCell>
+                    <TableBodyCell>
+                        {domain.threat_type
+                            ? domain.threat_type.charAt(0).toUpperCase() + domain.threat_type.substring(1)
+                            : "Unknown"}
+                    </TableBodyCell>
+                    <TableBodyCell>
+                        {domain.host ? domain.host.charAt(0).toUpperCase() + domain.host.substring(1) : "Unknown"}
+                    </TableBodyCell>
                     <TableBodyCell>{relativeTimeFromDate(new Date(domain.created_at * 1000))}</TableBodyCell>
                     <TableBodyCell>{relativeTimeFromDate(new Date(domain.updated_at * 1000))}</TableBodyCell>
                 </TableBodyRow>
