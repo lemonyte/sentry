b96dd72e012aed0aefcfe8d757da1a8c0cfdcc85
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2024-01-14 02:59:18 -0800

    Add stat cards

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
     import type { PageData } from "./$types";
-    import { Alert } from "flowbite-svelte";
+    import { DomainStatus } from "$lib/database";
+    import { Card } from "flowbite-svelte";
 
     export let data: PageData;
 </script>
 
 <div class="flex flex-col items-center min-h-screen">
-    <div class="p-8">
-        <Alert>
-            <span class="font-medium">Info alert!</span>
-            Change a few things up and try submitting again.
-        </Alert>
+    <div class="flex flex-row space-x-4" id="cards">
+        <Card>
+            <h3>Active domains</h3>
+            <p class="text-red-500">{data.domains.filter((domain) => domain.status === DomainStatus.active).length}</p>
+        </Card>
+        <Card>
+            <h3>Inactive domains</h3>
+            <p class="text-gray-500">
+                {data.domains.filter((domain) => domain.status === DomainStatus.inactive).length}
+            </p>
+        </Card>
+        <Card>
+            <h3>Domains actioned</h3>
+            <p class="text-green-500">TODO</p>
+        </Card>
     </div>
 </div>
+
+<style lang="postcss">
+    #cards h3 {
+        @apply text-xl;
+    }
+
+    #cards p {
+        @apply text-6xl font-bold self-center;
+    }
+</style>
