400b17ef8c5f88e3434bbf5efe11b7ec3503a42c
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2024-01-15 13:48:44 -0800

    Update svelte layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
     import "../app.css";
-    import { page } from '$app/stores';
-    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+    import { page } from "$app/stores";
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
+
     $: activeUrl = $page.url.pathname;
+    const navLinks = {
+        "/stats": "Statistics",
+        "/domains": "Domains",
+        "/admin": "Admin",
+    };
 </script>
-  
-<Navbar>
-<NavBrand href="/">
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Sentry</span>
-</NavBrand>
-<NavHamburger />
-<NavUl {activeUrl}>
-    <NavLi href="/stats">Statistics</NavLi>
-    <NavLi href="/domains">Domains</NavLi>
-    <NavLi href="/admin">Admin</NavLi>
-</NavUl>
+
+<Navbar class="!bg-gray-800">
+    <NavBrand href="/">
+        <span class="self-center whitespace-nowrap text-2xl font-bold dark:text-white">Sentry</span>
+    </NavBrand>
+    <NavHamburger />
+    <NavUl {activeUrl} ulClass="flex flex-col p-2 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:text-sm md:font-medium">
+        {#each Object.entries(navLinks) as [url, name]}
+            <NavLi href={url} class="text-lg transition-all">{name}</NavLi>
+        {/each}
+    </NavUl>
 </Navbar>
 
-<slot />
+<div class="mt-4 flex flex-col items-center">
+    <slot />
+</div>
--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -6,23 +6,21 @@
     export let data: PageData;
 </script>
 
-<div class="flex flex-col items-center min-h-screen">
-    <div class="flex flex-row space-x-4" id="cards">
-        <Card>
-            <h3>Active domains</h3>
-            <p class="text-red-500">{data.domains.filter((domain) => domain.status === DomainStatus.active).length}</p>
-        </Card>
-        <Card>
-            <h3>Inactive domains</h3>
-            <p class="text-gray-500">
-                {data.domains.filter((domain) => domain.status === DomainStatus.inactive).length}
-            </p>
-        </Card>
-        <Card>
-            <h3>Domains actioned</h3>
-            <p class="text-green-500">TODO</p>
-        </Card>
-    </div>
+<div class="flex flex-row space-x-4" id="cards">
+    <Card>
+        <h3>Active domains</h3>
+        <p class="text-red-500">{data.domains.filter((domain) => domain.status === DomainStatus.active).length}</p>
+    </Card>
+    <Card>
+        <h3>Inactive domains</h3>
+        <p class="text-gray-500">
+            {data.domains.filter((domain) => domain.status === DomainStatus.inactive).length}
+        </p>
+    </Card>
+    <Card>
+        <h3>Domains actioned</h3>
+        <p class="text-green-500">TODO</p>
+    </Card>
 </div>
 
 <style lang="postcss">
